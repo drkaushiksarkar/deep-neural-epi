be764184035ed76fcc1670d2e2735ed2c00497ef
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2019-06-10 16:00:17 +0000

    docs(tests): transformer_model tests v3

---
+++ src/tests/transformer_model_v3_d1091_c110.py
@@ -0,0 +1,25 @@
+"""Tests for transformer_model v3d1091y2019."""
+import pytest
+import torch
+import numpy as np
+
+
+class TestTransformerModel_v3d1091y2019:
+    def test_init(self):
+        config = {"domain": "transformer_model", "v": 3}
+        assert config["v"] == 3
+
+    def test_forward(self):
+        x = torch.randn(12, 24)
+        y = torch.nn.functional.gelu(x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [torch.randn(10) for _ in range(9)]
+        assert len(batch) == 9
+
+    def test_metric(self):
+        pred = torch.randn(24)
+        target = torch.randn(24)
+        loss = torch.nn.functional.mse_loss(pred, target)
+        assert loss.item() >= 0
