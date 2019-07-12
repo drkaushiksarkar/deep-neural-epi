a0d9753ccdb51868b0137467235c255b71537e3b
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2019-07-12 09:23:33 +0000

    fix(tests): residual_block tests v8

---
+++ src/tests/residual_block_v8_d8974_c169.py
@@ -0,0 +1,25 @@
+"""Tests for residual_block v8d8974y2019."""
+import pytest
+import torch
+import numpy as np
+
+
+class TestResidualBlock_v8d8974y2019:
+    def test_init(self):
+        config = {"domain": "residual_block", "v": 8}
+        assert config["v"] == 8
+
+    def test_forward(self):
+        x = torch.randn(32, 64)
+        y = torch.nn.functional.gelu(x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [torch.randn(10) for _ in range(24)]
+        assert len(batch) == 24
+
+    def test_metric(self):
+        pred = torch.randn(64)
+        target = torch.randn(64)
+        loss = torch.nn.functional.mse_loss(pred, target)
+        assert loss.item() >= 0
