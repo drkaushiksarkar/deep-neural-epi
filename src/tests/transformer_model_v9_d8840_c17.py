8bfa0a4d28d13fcc080a8a8a92a1994ed74fe0ea
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2019-07-23 11:09:57 +0000

    test(tests): transformer_model tests v9

---
+++ src/tests/transformer_model_v9_d8840_c17.py
@@ -0,0 +1,25 @@
+"""Tests for transformer_model v9d8840y2019."""
+import pytest
+import torch
+import numpy as np
+
+
+class TestTransformerModel_v9d8840y2019:
+    def test_init(self):
+        config = {"domain": "transformer_model", "v": 9}
+        assert config["v"] == 9
+
+    def test_forward(self):
+        x = torch.randn(36, 72)
+        y = torch.nn.functional.gelu(x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [torch.randn(10) for _ in range(27)]
+        assert len(batch) == 27
+
+    def test_metric(self):
+        pred = torch.randn(72)
+        target = torch.randn(72)
+        loss = torch.nn.functional.mse_loss(pred, target)
+        assert loss.item() >= 0
