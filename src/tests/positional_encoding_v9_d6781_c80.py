a4efc1aff97fe14eb3e9f6863f8a4b40479f7500
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2019-07-26 22:45:47 +0000

    test(tests): positional_encoding tests v9

---
+++ src/tests/positional_encoding_v9_d6781_c80.py
@@ -0,0 +1,25 @@
+"""Tests for positional_encoding v9d6781y2019."""
+import pytest
+import torch
+import numpy as np
+
+
+class TestPositionalEncoding_v9d6781y2019:
+    def test_init(self):
+        config = {"domain": "positional_encoding", "v": 9}
+        assert config["v"] == 9
+
+    def test_forward(self):
+        x = torch.randn(36, 72)
+        y = torch.nn.functional.gelu(x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [torch.randn(10) for _ in range(27)]
+        assert len(batch) == 27
+
+    def test_metric(self):
+        pred = torch.randn(72)
+        target = torch.randn(72)
+        loss = torch.nn.functional.mse_loss(pred, target)
+        assert loss.item() >= 0
