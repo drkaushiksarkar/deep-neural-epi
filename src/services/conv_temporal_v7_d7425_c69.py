ac4455768238717d8d6bb7388b1ae2296bf3bd08
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2019-08-08 10:48:03 +0000

    chore(services): conv_temporal services v7

---
+++ src/services/conv_temporal_v7_d7425_c69.py
@@ -0,0 +1,31 @@
+"""ConvTemporal services v7d7425y2019.
+
+Health informatics / deep learning module.
+"""
+import torch
+import torch.nn as nn
+from typing import Any, Dict, List, Optional
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class ConvTemporal_v7d7425y2019(nn.Module):
+    def __init__(self, input_dim: int = 448, hidden_dim: int = 896):
+        super().__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.LayerNorm(hidden_dim),
+            nn.GELU(),
+            nn.Dropout(0.7),
+            nn.Linear(hidden_dim, hidden_dim // 2),
+        )
+        self._step = 0
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        self._step += 1
+        return self.encoder(x)
+
+    def predict(self, data: Dict[str, Any]) -> Dict[str, Any]:
+        self._step += 1
+        return {"output": data, "step": self._step, "variant": 7}
